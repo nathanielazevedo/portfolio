210a6b39a10f43a7ea858000b99a3af5e7bac8e3
nathanielazevedo <nathanielazevedo@hotmail.com>
2021-03-08 20:19:03 -0800

    first push

---
+++ profile.js
@@ -0,0 +1,10 @@
+const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper');
+
+portfolioItems.forEach((e) => {
+    e.addEventListener('mouseover', () => {
+        e.childNodes[1].classList.add('img-darken')
+    })
+    e.addEventListener('mouseout', () => {
+        e.childNodes[1].classList.remove('img-darken')
+    })
+})
