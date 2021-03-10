3a994321c6abf72cb4ce46896b3d9eedef82c7fa
nathanielazevedo <nathanielazevedo@hotmail.com>
2021-03-09 16:55:20 -0800

    new projects

--- profile.js
+++ profile.js
@@ -1,10 +1,12 @@
-const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper');
+/** @format */
+
+const portfolioItems = document.querySelectorAll(".portfolio-item-wrapper");
 
 portfolioItems.forEach((e) => {
-    e.addEventListener('mouseover', () => {
-        e.childNodes[1].classList.add('img-darken')
-    })
-    e.addEventListener('mouseout', () => {
-        e.childNodes[1].classList.remove('img-darken')
-    })
-})
+  e.addEventListener("mouseover", () => {
+    e.childNodes[1].classList.add("img-darken");
+  });
+  e.addEventListener("mouseout", () => {
+    e.childNodes[1].classList.remove("img-darken");
+  });
+});
